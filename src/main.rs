2c340f61fdf09c3636761114addd20f3a1ef53d7
lovenangelo <lovenangelo.dev@gmail.com>
2025-02-11 21:08:27 +0800

    feat: more sounds

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,11 @@ struct Index;
 struct PomodoroTemplate;
 
 #[derive(Template)]
-#[template(path = "bugle_sound.html")]
-struct BugleSoundTemplate;
+#[template(path = "sound.html")]
+struct SoundTemplate<'a> {
+    path: &'a str,
+    id: &'a str,
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -29,7 +32,34 @@ async fn pomodoro() -> impl Responder {
 
 #[get("/bugle-sound")]
 async fn bugle_sound() -> impl Responder {
-    BugleSoundTemplate
+    SoundTemplate {
+        id: "bugle",
+        path: "http://127.0.0.1:8080/pomodoro/audio/reveille.mp3",
+    }
+}
+
+#[get("/button-sound-1")]
+async fn button_sound_1() -> impl Responder {
+    SoundTemplate {
+        id: "button1",
+        path: "http://127.0.0.1:8080/pomodoro/audio/button-sound-1.mp3",
+    }
+}
+
+#[get("/button-sound-2")]
+async fn button_sound_2() -> impl Responder {
+    SoundTemplate {
+        id: "button2",
+        path: "http://127.0.0.1:8080/pomodoro/audio/button-sound-2.mp3",
+    }
+}
+
+#[get("/ding-sound")]
+async fn ding_sound() -> impl Responder {
+    SoundTemplate {
+        id: "ding",
+        path: "http://127.0.0.1:8080/pomodoro/audio/ding.mp3",
+    }
 }
 
 #[get("/audio/{filename}")]
@@ -48,6 +78,9 @@ async fn main() -> std::io::Result<()> {
             web::scope("/pomodoro")
                 .service(pomodoro)
                 .service(bugle_sound)
+                .service(button_sound_1)
+                .service(button_sound_2)
+                .service(ding_sound)
                 .service(get_audio),
         )
     })
